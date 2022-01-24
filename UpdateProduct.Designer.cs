063a9444395e3116035b68f232fae40fa93a7162
GeneralSingko <74855892+GeneralSingko@users.noreply.github.com>
2022-01-24 13:58:44 +0800

    Update Product Design
    the smtp part

---
+++ UpdateProduct.Designer.cs
@@ -0,0 +1,260 @@
+﻿
+namespace POS_DDT
+{
+    partial class UpdateProduct
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.cbxSSL = new System.Windows.Forms.CheckBox();
+            this.txtSender = new System.Windows.Forms.TextBox();
+            this.txtBody = new System.Windows.Forms.TextBox();
+            this.txtSubject = new System.Windows.Forms.TextBox();
+            this.txtRecipient = new System.Windows.Forms.TextBox();
+            this.txtPortNo = new System.Windows.Forms.TextBox();
+            this.txtSmptserver = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnsendmail = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(347, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(271, 39);
+            this.label2.TabIndex = 55;
+            this.label2.Text = "Update Product";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(177, 297);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 24);
+            this.label1.TabIndex = 56;
+            this.label1.Text = "Recipient Email";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(177, 339);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 24);
+            this.label3.TabIndex = 57;
+            this.label3.Text = "Subject";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(177, 209);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(147, 24);
+            this.label4.TabIndex = 58;
+            this.label4.Text = "Server Port No";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(177, 372);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 24);
+            this.label5.TabIndex = 59;
+            this.label5.Text = "Body";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(177, 169);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(133, 24);
+            this.label6.TabIndex = 60;
+            this.label6.Text = "SMTP Server";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(177, 128);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(174, 24);
+            this.label7.TabIndex = 61;
+            this.label7.Text = "Sender Password";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(177, 85);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(136, 24);
+            this.label8.TabIndex = 62;
+            this.label8.Text = "Sender Email";
+            // 
+            // cbxSSL
+            // 
+            this.cbxSSL.AutoSize = true;
+            this.cbxSSL.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbxSSL.Location = new System.Drawing.Point(354, 250);
+            this.cbxSSL.Name = "cbxSSL";
+            this.cbxSSL.Size = new System.Drawing.Size(55, 22);
+            this.cbxSSL.TabIndex = 63;
+            this.cbxSSL.Text = "SSL";
+            this.cbxSSL.UseVisualStyleBackColor = true;
+            // 
+            // txtSender
+            // 
+            this.txtSender.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSender.Location = new System.Drawing.Point(354, 78);
+            this.txtSender.Name = "txtSender";
+            this.txtSender.Size = new System.Drawing.Size(394, 31);
+            this.txtSender.TabIndex = 64;
+            // 
+            // txtBody
+            // 
+            this.txtBody.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBody.Location = new System.Drawing.Point(354, 377);
+            this.txtBody.Multiline = true;
+            this.txtBody.Name = "txtBody";
+            this.txtBody.Size = new System.Drawing.Size(394, 205);
+            this.txtBody.TabIndex = 65;
+            // 
+            // txtSubject
+            // 
+            this.txtSubject.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSubject.Location = new System.Drawing.Point(354, 332);
+            this.txtSubject.Name = "txtSubject";
+            this.txtSubject.Size = new System.Drawing.Size(394, 31);
+            this.txtSubject.TabIndex = 66;
+            // 
+            // txtRecipient
+            // 
+            this.txtRecipient.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRecipient.Location = new System.Drawing.Point(354, 290);
+            this.txtRecipient.Name = "txtRecipient";
+            this.txtRecipient.Size = new System.Drawing.Size(394, 31);
+            this.txtRecipient.TabIndex = 67;
+            // 
+            // txtPortNo
+            // 
+            this.txtPortNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPortNo.Location = new System.Drawing.Point(354, 202);
+            this.txtPortNo.Name = "txtPortNo";
+            this.txtPortNo.Size = new System.Drawing.Size(394, 31);
+            this.txtPortNo.TabIndex = 68;
+            // 
+            // txtSmptserver
+            // 
+            this.txtSmptserver.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSmptserver.Location = new System.Drawing.Point(354, 162);
+            this.txtSmptserver.Name = "txtSmptserver";
+            this.txtSmptserver.Size = new System.Drawing.Size(394, 31);
+            this.txtSmptserver.TabIndex = 69;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(354, 121);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(394, 31);
+            this.txtPassword.TabIndex = 70;
+            // 
+            // btnsendmail
+            // 
+            this.btnsendmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsendmail.Location = new System.Drawing.Point(354, 588);
+            this.btnsendmail.Name = "btnsendmail";
+            this.btnsendmail.Size = new System.Drawing.Size(92, 39);
+            this.btnsendmail.TabIndex = 71;
+            this.btnsendmail.Text = "Send";
+            this.btnsendmail.UseVisualStyleBackColor = true;
+            this.btnsendmail.Click += new System.EventHandler(this.btnsendmail_Click);
+            // 
+            // UpdateProduct
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(163)))), ((int)(((byte)(191)))), ((int)(((byte)(221)))));
+            this.Controls.Add(this.btnsendmail);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtSmptserver);
+            this.Controls.Add(this.txtPortNo);
+            this.Controls.Add(this.txtRecipient);
+            this.Controls.Add(this.txtSubject);
+            this.Controls.Add(this.txtBody);
+            this.Controls.Add(this.txtSender);
+            this.Controls.Add(this.cbxSSL);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Name = "UpdateProduct";
+            this.Size = new System.Drawing.Size(991, 709);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.CheckBox cbxSSL;
+        private System.Windows.Forms.TextBox txtSender;
+        private System.Windows.Forms.TextBox txtBody;
+        private System.Windows.Forms.TextBox txtSubject;
+        private System.Windows.Forms.TextBox txtRecipient;
+        private System.Windows.Forms.TextBox txtPortNo;
+        private System.Windows.Forms.TextBox txtSmptserver;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnsendmail;
+    }
+}
---
+++ UpdateProduct.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Mail;
+
+namespace POS_DDT
+{
+    public partial class UpdateProduct : UserControl
+    {
+        public UpdateProduct()
+        {
+            InitializeComponent();
+        }
+
+        private void btnsendmail_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SmtpClient clientDetails = new SmtpClient();
+                clientDetails.Port = Convert.ToInt32(txtPortNo.Text.Trim());
+                clientDetails.Host = txtSmptserver.Text.Trim();
+                clientDetails.EnableSsl = cbxSSL.Checked;
+                clientDetails.DeliveryMethod = SmtpDeliveryMethod.Network;
+                clientDetails.UseDefaultCredentials = false;
+                clientDetails.Credentials = new NetworkCredential(txtSender.Text.Trim(), txtPassword.Text.Trim());
+
+                MailMessage mailDetails = new MailMessage();
+                mailDetails.From = new MailAddress(txtSender.Text.Trim());
+                mailDetails.To.Add(txtRecipient.Text.Trim());
+                mailDetails.Subject = txtSubject.Text.Trim();
+                mailDetails.IsBodyHtml = cbxSSL.Checked;
+                mailDetails.Body = txtBody.Text.Trim();
+
+                clientDetails.Send(mailDetails);
+
+
+                MessageBox.Show("Email Sent");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
